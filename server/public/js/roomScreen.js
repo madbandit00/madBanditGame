728d066a97999433d0f3a3cb229370c4dbf7af7e
Ikmal <ikmal_rani@yahoo.com>
2021-03-22 16:03:20 +0800

    updated project to support heroku

--- server/public/js/roomScreen.js
+++ server/public/js/roomScreen.js
@@ -5,7 +5,7 @@ class RoomScreen extends Phaser.Scene {
 
     preload ()
     {
-        this.load.html('nameform', 'assets/text/loginform.html');
+        //this.load.html('nameform', 'assets/text/loginform.html');
         //this.load.image('pic', 'assets/pics/turkey-1985086.jpg');
     }
     
@@ -13,56 +13,28 @@ class RoomScreen extends Phaser.Scene {
     {
         //this.add.image(400, 300, 'pic');
     
-        var text = this.add.text(10, 10, 'Please login to play', { color: 'white', fontFamily: 'Arial', fontSize: '32px '});
+        var text = this.add.text(10, 10, 'Enter Code', { color: 'white', fontFamily: 'Arial', fontSize: '32px '});
+
+        this.add.text(10, 10, 'Code:', { font: '32px Courier', fill: '#ffffff' });
+
+        var textEntry = this.add.text(10, 50, '', { font: '32px Courier', fill: '#ffff00' });
     
-        var element = this.add.dom(400, 600).createFromCache('nameform');
+        this.input.keyboard.on('keydown', function (event) {
     
-        //element.setPerspective(800);
-    
-        element.addListener('click');
-    
-        element.on('click', function (event) {
-    
-            if (event.target.name === 'loginButton')
+            if (event.keyCode === 8 && textEntry.text.length > 0)
             {
-                var inputUsername = this.getChildByName('username');
-                var inputPassword = this.getChildByName('password');
-    
-                //  Have they entered anything?
-                if (inputUsername.value !== '' && inputPassword.value !== '')
-                {
-                    //  Turn off the click events
-                    this.removeListener('click');
-    
-                    //  Tween the login form out
-                    this.scene.tweens.add({ targets: element.rotate3d, x: 1, w: 90, duration: 3000, ease: 'Power3' });
-    
-                    this.scene.tweens.add({ targets: element, scaleX: 2, scaleY: 2, y: 300, duration: 0, ease: 'Power3',
-                        onComplete: function ()
-                        {
-                            element.setVisible(false);
-                        }
-                    });
-    
-                    //  Populate the text with whatever they typed in as the username!
-                    text.setText('Welcome ' + inputUsername.value);
-                }
-                else
-                {
-                    //  Flash the prompt
-                    this.scene.tweens.add({ targets: text, alpha: 0.1, duration: 0, ease: 'Power3', yoyo: true });
-                }
+                textEntry.text = textEntry.text.substr(0, textEntry.text.length - 1);
+            }
+            else if (event.keyCode === 32 || (event.keyCode >= 48 && event.keyCode < 90))
+            {
+                textEntry.text += event.key;
             }
     
         });
-     
-        this.tweens.add({
-            targets: element,
-            y: 300,
-            duration: 0,
-            ease: 'Power3'
-        });
-    }
+
+        var text = this.add.text(10, 100, 'Join', { color: 'white', fontFamily: 'Arial', fontSize: '32px '});
+    
+        
 
 }
 
