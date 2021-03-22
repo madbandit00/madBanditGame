0aed074eb77b7388146d2beeeb58cd9c75faa417
Ikmal <ikmal_rani@yahoo.com>
2021-03-22 20:56:25 +0800

    updated project to support heroku

--- server/public/js/roomScreen.js
+++ server/public/js/roomScreen.js
@@ -18,17 +18,11 @@ class RoomScreen extends Phaser.Scene {
     {
         //this.add.image(400, 300, 'pic');
     
-        var text0 = this.add.text(10, 10, 'Create', { color: 'white', fontFamily: 'Arial', fontSize: '32px '});
-
-        var text1 = this.add.text(10, 100, 'Join', { color: 'white', fontFamily: 'Arial', fontSize: '32px '});
-        
-        this.add.text(10, 50, 'Enter Code:', { font: '32px Courier', fill: '#ffffff' });
-
-        const text3 = this.add.text(300, 80, 'Enter Code', { fixedWidth: 175, fixedHeight: 50 })
-        text3.setOrigin(0.5, 0.5).setInteractive()
+        const text = this.add.text(400, 300, 'Hello World', { fixedWidth: 150, fixedHeight: 36 })
+        text.setOrigin(0.5, 0.5)
     
-        text3.on('pointerdown', () => {
-            this.rexUI.edit(text3)
+        text.setInteractive().on('pointerdown', () => {
+            this.rexUI.edit(text)
         })
 
         //var textEntry = this.add.text(110, 50, '', { font: '32px Courier', fill: '#ffff00' });
