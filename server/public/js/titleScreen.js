9efd21c17a8899e766ebe697be89db7ea737cce9
Ikmal <ikmal_rani@yahoo.com>
2021-03-22 14:53:43 +0800

    updated project to support heroku

--- server/public/js/titleScreen.js
+++ server/public/js/titleScreen.js
@@ -11,9 +11,9 @@ class TitleScreen extends Phaser.Scene {
       this.add.text(20, 20, "SMK Sonata...an Ikmal Game").setScale(2, 2);
       //this.dealText = this.add.text(75, 350, ['DEAL CARDS']).setFontSize(18).setFontFamily('Trebuchet MS').setColor('#00ffff').setInteractive();
       
-      let random = this.add.text(120, 60, "Play Random").setFontSize(80);
+      let random = this.add.text(120, 60, "Play Random").setFontSize(80).setInteractive();
 
-      let withFriend = this.add.text(120, 120, "Play With Friend").setFontSize(80);
+      let withFriend = this.add.text(120, 120, "Play With Friend").setFontSize(80).setInteractive();
       
       console.log("titleworking");
 
