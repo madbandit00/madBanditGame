6cbab2843264d3386dbdc9830cd1b2fcda17e90f
Ikmal <ikmal_rani@yahoo.com>
2021-03-23 22:33:43 +0800

    updated project to support heroku

--- server/public/js/roomScreen.js
+++ server/public/js/roomScreen.js
@@ -28,6 +28,7 @@ class RoomScreen extends Phaser.Scene {
 
         function handleInit(number) {
             playerNumber = number;
+            console.log('init received')
           }
 
         function handleGameCode(gameCode) {
