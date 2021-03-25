badd5cb59899140a3036f0403864a6c9e0b2522d
Ikmal <ikmal_rani@yahoo.com>
2021-03-25 13:28:31 +0800

    updated project to support heroku

--- server/public/js/roomScreen.js
+++ server/public/js/roomScreen.js
@@ -32,7 +32,7 @@ class RoomScreen extends Phaser.Scene {
             console.log('init received')
           }
 
-        let text0 = this.add.text(10, 10, 'Create new:', { color: 'white', fontFamily: 'Arial', fontSize: '32px '});
+        let text0 = this.add.text(10, 100, 'Create new:', { color: 'white', fontFamily: 'Arial', fontSize: '32px '});
 
         function handleGameCode(gameCode) {
 
@@ -59,7 +59,7 @@ class RoomScreen extends Phaser.Scene {
         })
 
        
-        this.add.text(10, 85, 'Enter Code:', { font: '32px Courier', fill: '#ffffff' });
+        this.add.text(10, 185, 'Enter Code:', { font: '32px Courier', fill: '#ffffff' });
 
         var text1 = this.add.text(10, 155, 'Join', { color: 'white', fontFamily: 'Arial', fontSize: '32px '});
 
@@ -68,7 +68,7 @@ class RoomScreen extends Phaser.Scene {
             joinGame()            
         })
 
-        let text3 = this.add.text(350, 105, 'Click', { fixedWidth: 200, fixedHeight: 75 })
+        let text3 = this.add.text(350, 205, 'Click', { fixedWidth: 200, fixedHeight: 75 })
         text3.setOrigin(0.5, 0.5);
     
         text3.setInteractive().on('pointerdown', () => {
