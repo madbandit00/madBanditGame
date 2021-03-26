cd672b16e50858e800f06fde0a412ff86cb32b5c
Ikmal <ikmal_rani@yahoo.com>
2021-03-26 18:42:40 +0800

    updated project to support heroku

--- server/authoritative_server/js/game.js
+++ server/authoritative_server/js/game.js
@@ -215,7 +215,7 @@ function create() {
 
     function playerAorB() {
 
-      if (socket.id === playersDetect.length[0] ) {
+      if (playersDetect.length === playersDetect.length[0] ) {
 
         self.isPlayerA = true;
         console.log('Player A ID: ' + socket.id);
