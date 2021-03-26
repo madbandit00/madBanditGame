57e0f11ad1eeb1703ecfacd51f9b6da9b14afb90
Ikmal <ikmal_rani@yahoo.com>
2021-03-26 20:27:46 +0800

    updated project to support heroku

--- server/public/js/smkSonata.js
+++ server/public/js/smkSonata.js
@@ -252,25 +252,25 @@ class SMKSonata extends Phaser.Scene {
 
         ]
 
-        for (let i = 0; i < S0.length; i++) {
+        // for (let i = 0; i < S0.length; i++) {
 
 
-            if (self.confirmedTextures[0][0] == S0[i].textureKey) {
-                playerSpriteA = S0[i].sprites;
+        //     if (self.confirmedTextures[0][0] == S0[i].textureKey) {
+        //         playerSpriteA = S0[i].sprites;
                 
-            }
+        //     }
 
-            if (self.confirmedTextures[0][1] == S0[i].textureKey) {
-                playerSpriteB = S0[i].sprites;
+        //     if (self.confirmedTextures[0][1] == S0[i].textureKey) {
+        //         playerSpriteB = S0[i].sprites;
                 
-            }
+        //     }
 
-        }
+        // }
         
 
         console.log(self.confirmedTextures[0][0].toString());
 
-        console.log(self.confirmedTextures[0][1].toString());
+        //console.log(self.confirmedTextures[0][1].toString());
 
     
     }, 10000);
