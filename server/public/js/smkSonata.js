6dd9fe01c5dfb716946095f8ea7b8d1d9a8229c8
Ikmal <ikmal_rani@yahoo.com>
2021-04-07 14:10:40 +0800

    updated project to support heroku

--- server/public/js/smkSonata.js
+++ server/public/js/smkSonata.js
@@ -78,7 +78,23 @@ class SMKSonata extends Phaser.Scene {
 
     self.socket.emit('whatTexture');
 
+    this.socket.on('texturePickedPrivate', textureCheckPrivate);
+
     this.socket.on('texturePicked', textureCheck);
+
+    function textureCheckPrivate(ConfirmedTexture) {
+    
+        //confirmedTexture = ConfirmedTexture;
+                    
+        console.log(ConfirmedTexture + ' logTest');
+
+        console.log(self.confirmedTextures);
+
+        self.confirmedTextures.push(ConfirmedTexture);
+
+        console.log(self.confirmedTextures);
+
+    };
     
     function textureCheck(ConfirmedTexture) {
     
