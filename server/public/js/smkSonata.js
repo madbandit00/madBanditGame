cd161a78ec46fbd3afee8134cfba0f52966ff39c
Ikmal <ikmal_rani@yahoo.com>
2021-04-07 23:44:29 +0800

    updated project to support heroku

--- server/public/js/smkSonata.js
+++ server/public/js/smkSonata.js
@@ -64,7 +64,7 @@ class SMKSonata extends Phaser.Scene {
         this.isPlayerA = false;
         self.isPlayerA  = yesOrno;
         console.log('player A: ' + self.isPlayerA);
-        //return self.isPlayerA
+        return self.isPlayerA
       }
  
     console.log('player A: ' + self.isPlayerA);
