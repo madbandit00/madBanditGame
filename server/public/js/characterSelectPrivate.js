322b7904fde1d182b5760dfbf9e8392a433eb994
Ikmal <ikmal_rani@yahoo.com>
2021-04-09 12:48:18 +0800

    updated project to support heroku

--- server/public/js/characterSelectPrivate.js
+++ server/public/js/characterSelectPrivate.js
@@ -101,7 +101,7 @@ class CharacterSelectPrivate extends Phaser.Scene {
 
       var r4 = this.add.rectangle(1200, 200, 148, 148, 0x6666ff);
 
-      let confirmText = this.add.text(1140, 305, "Confirm").setScale(2, 2).setInteractive();
+      let confirmText = this.add.text(1135, 305, "Confirm").setScale(2, 2).setInteractive();
 
       confirmText.on('pointerdown', function (pointer) {
 
