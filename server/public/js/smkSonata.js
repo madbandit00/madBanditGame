84de843b0fcd922c1ba8cbf340db0c981bacdd25
Ikmal <ikmal_rani@yahoo.com>
2021-04-13 15:00:12 +0800

    updated project to support heroku

--- server/public/js/smkSonata.js
+++ server/public/js/smkSonata.js
@@ -445,8 +445,9 @@ class SMKSonata extends Phaser.Scene {
         // })
 
         this.socket.on('dealCards', function () {
-            self.dealCards()
-            self.dealText.setColor('#FFA500').disableInteractive();         
+            self.dealCards();
+            self.dealText.setColor('#FFFFFF.');
+            self.dealText.disableInteractive();         
         })
 
         this.socket.on('cardPlayed', function (gameObject, isPlayerA) {
