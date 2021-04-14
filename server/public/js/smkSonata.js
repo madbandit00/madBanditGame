678feb6b99aa39d5bee9c01caf1a01992786c4b1
Ikmal <ikmal_rani@yahoo.com>
2021-04-14 15:55:28 +0800

    updated project to support heroku

--- server/public/js/smkSonata.js
+++ server/public/js/smkSonata.js
@@ -600,13 +600,13 @@ class SMKSonata extends Phaser.Scene {
 
             if (self.dropZone.data.values.cards + self.dropZone2.data.values.cards == 5){
 
-                renderQuestions.destroy();
+                self.renderQuestions.destroy();
                 self.dropZone.data.values.cards = 0;
                 self.dropZone2.data.values.cards = 0;
 
             }
 
-            //console.log("cards dropped" + (self.dropZone.data.values.cards + self.dropZone2.data.values.cards))
+            console.log("cards dropped" + (self.dropZone.data.values.cards + self.dropZone2.data.values.cards))
                 
         }
         
