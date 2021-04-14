7d1bd491b75122d3d79635bfeda665f9deb9531d
Ikmal <ikmal_rani@yahoo.com>
2021-04-14 17:00:39 +0800

    updated project to support heroku

--- server/public/js/smkSonata.js
+++ server/public/js/smkSonata.js
@@ -219,7 +219,7 @@ class SMKSonata extends Phaser.Scene {
         let a = self.add.text(x1, y1, answerA).setFontSize(18).setFontFamily('Trebuchet MS').setColor('#00ffff');
         let b = self.add.text(x2, y2, answerB).setFontSize(18).setFontFamily('Trebuchet MS').setColor('#00ffff');
         
-        return a, b;
+        return a;
         
     };
 
