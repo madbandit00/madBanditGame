caf69c5360b9aa64f22d8a904670f3c485bf0029
Ikmal <ikmal_rani@yahoo.com>
2021-04-23 16:43:40 +0800

    updated project to support heroku

--- server/public/js/smkSonata.js
+++ server/public/js/smkSonata.js
@@ -677,13 +677,15 @@ class SMKSonata extends Phaser.Scene {
         if (scores.blue > 29){
             console.log ('CCS wins')
             //this.scene.start("characterSelect", Socket);
-            this.scene.start("winnerScreen");
+            self.scene.start("winnerScreen");
+            self.scene.remove('smkSonata');
             
         }
 
         if (scores.red > 29){
             console.log ('JOJO wins')
-            this.scene.start("winnerScreen2");
+            self.scene.start("winnerScreen2");
+            self.scene.remove('smkSonata');
         }
         });
 
