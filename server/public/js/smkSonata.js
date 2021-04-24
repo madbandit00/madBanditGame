a2cf388b94b3bd4e746f76df7e662a8814ec3348
Ikmal <ikmal_rani@yahoo.com>
2021-04-24 13:10:03 +0800

    updated project to support heroku

--- server/public/js/smkSonata.js
+++ server/public/js/smkSonata.js
@@ -568,8 +568,8 @@ class SMKSonata extends Phaser.Scene {
             //self.dealText.setColor('#00ffff');
             self.dropZone.data.values.cards = 0;
             self.dropZone2.data.values.cards = 0;
-            this.answerAcheck = false;
-            this.answerBcheck = false;
+            self.answerAcheck = false;
+            self.answerBcheck = false;
             self.dealCards();
 
             }
@@ -636,8 +636,8 @@ class SMKSonata extends Phaser.Scene {
             //self.dealText.setColor('#00ffff');
             self.dropZone.data.values.cards = 0;
             self.dropZone2.data.values.cards = 0;
-            this.answerAcheck = false;
-            this.answerBcheck = false;
+            self.answerAcheck = false;
+            self.answerBcheck = false;
             self.dealCards();
 
            
