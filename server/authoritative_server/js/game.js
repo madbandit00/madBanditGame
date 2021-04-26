1250961787a49de8efc34a3ce36acea435d6b6d7
Ikmal <ikmal_rani@yahoo.com>
2021-04-26 22:51:31 +0800

    updated project to support heroku

--- server/authoritative_server/js/game.js
+++ server/authoritative_server/js/game.js
@@ -267,6 +267,8 @@ function create() {
     if (playersDetect.length > 2 ) {
 
       self.confirmedTexture = [];
+      io.emit('texturePicked', self.confirmedTexture);
+      console.log(self.confirmedTexture);
     }
 
     socket.on('textureKey', checkTexture);
