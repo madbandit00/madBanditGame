eaf7b21a80fe27f069fd955069729ecc8e67f8a2
Ikmal <ikmal_rani@yahoo.com>
2021-05-02 17:41:42 +0800

    updated project to support heroku

--- server/public/js/smkSonata.js
+++ server/public/js/smkSonata.js
@@ -299,16 +299,6 @@ class SMKSonata extends Phaser.Scene {
 
         this.scene.remove('roomScreen');
 
-        this.socket.on('starLocation', function (starLocation) {
-            console.log(self.star)
-            if (!self.star) {
-     
-                self.star = self.add.image(starLocation.x, starLocation.y, 'star');
-            } else {
-                self.star.setPosition(starLocation.x, starLocation.y);
-            }
-            });
-
     
     }, 9000);
       
@@ -720,7 +710,15 @@ class SMKSonata extends Phaser.Scene {
         }
         });
 
-
+        this.socket.on('starLocation', function (starLocation) {
+        console.log(self.star)
+        if (!self.star) {
+ 
+            self.star = self.add.image(starLocation.x, starLocation.y, 'star');
+        } else {
+            self.star.setPosition(starLocation.x, starLocation.y);
+        }
+        });
 
         this.cursors = this.input.keyboard.createCursorKeys();
         this.leftKeyPressed = false;
