c3111bb3f1fc9be08100a734cdb535c9b65fd601
Ikmal <ikmal_rani@yahoo.com>
2021-05-04 00:12:38 +0800

    updated project to support heroku

--- server/authoritative_server/js/game.js
+++ server/authoritative_server/js/game.js
@@ -343,6 +343,7 @@ function create() {
     socket.on('dealCards', function () {
         // send the star object to the new player
         socket.emit('starLocation', { x: self.star.x, y: self.star.y });
+        io.emit('playerUpdates', players);
         io.emit('dealCards');
     });
 
@@ -397,7 +398,7 @@ function update() {
   });
   
   this.physics.world.wrap(this.players, 5);
-  io.emit('playerUpdates', players);
+  //io.emit('playerUpdates', players);
 }
 
 function randomPosition(max) {
