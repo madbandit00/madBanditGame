602561c7540ea23956f99d0b406434e148cc7910
Ikmal <ikmal_rani@yahoo.com>
2021-05-04 15:34:56 +0800

    updated project to support heroku

--- server/authoritative_server/js/game.js
+++ server/authoritative_server/js/game.js
@@ -75,7 +75,7 @@ function create() {
 
   this.input.addPointer(2);
 
-  this.physics.add.overlap(this.players, self.star, function (star, player) {
+  this.physics.add.overlap(this.players, this.star, function (star, player) {
     if (players[player.playerId].team === 'red' ) {
       self.scores.red += 10;
       scoreCheckerRed += 10;
@@ -346,7 +346,7 @@ function create() {
         io.emit('dealCards');
         //self.star = self.physics.add.image(1100, 400, 'star');
 
-        self.star = self.physics.add.image(1100, 400, 'star');
+        this.star = this.physics.add.image(1100, 400, 'star');
 
         socket.emit('starLocation', { x: self.star.x, y: self.star.y })
 
