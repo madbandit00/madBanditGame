ea4bd8442af94df22a5750b44aca39fbcc40b075
Ikmal <ikmal_rani@yahoo.com>
2021-05-12 12:01:55 +0800

    updated project to support heroku

--- server/public/js/smkSonata.js
+++ server/public/js/smkSonata.js
@@ -167,8 +167,8 @@ class SMKSonata extends Phaser.Scene {
 
     //this.dealer = new Dealer(this);
 
-    this.blueScoreText = this.add.text(20, 16, '', { fontSize: '32px', fill: '#0000FF' });
-    this.redScoreText = this.add.text(1060, 16, '', { fontSize: '32px', fill: '#FF0000' });
+    this.blueScoreText = this.add.text(100, 16, '', { fontSize: '32px', fill: '#0000FF' });
+    this.redScoreText = this.add.text(1100, 16, '', { fontSize: '32px', fill: '#FF0000' });
 
     let questions = [
         
